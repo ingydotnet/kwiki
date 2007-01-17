1324762ec1fe37bb56230cf72fb8e9d54f461b21
ambs <ambs@4b3b33bc-d223-0410-9309-b363ae52eedd>
2007-01-17 19:55:23 +0000

    Added theme
    git-svn-id: http://svn.kwiki.org/kwiki/trunk@52 4b3b33bc-d223-0410-9309-b363ae52eedd

---
+++ src/plugins/ambs/Kwiki-Theme-Sonia/lib/Kwiki/Theme/Sonia.pm
@@ -0,0 +1,482 @@
+package Kwiki::Theme::Sonia;
+use Kwiki::Theme -Base;
+our $VERSION = '0.01';
+
+const theme_id => 'sonia';
+const class_title => 'Sonia Theme';
+
+__DATA__
+
+=head1 NAME 
+
+Kwiki::Theme::Sonia - Kwiki Sonia Theme
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 AUTHOR
+
+Based on Sonia Template at http://www.oswd.org/ by LanVacation.
+Adaptation by Alberto Simoes <ambs@cpan.org>
+
+=head1 COPYRIGHT
+
+Copyright (c) 2007. Alberto Simoes. All rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+See http://www.perl.com/perl/misc/Artistic.html
+
+=cut
+__theme/sonia/css/clanak.gif__
+R0lGODlhFwAnAMQAAN3d3ezs7O3t7e7u7u/v7/Dw8PLy8vPz8/T09PX19fb29vf39/j4+Pn5+fr6
++vv7+/z8/P39/f7+/v///wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACwAAAAA
+FwAnAAAIyQAnCBxIsKDBgwgTKlzIsKHDhxAjSpxIsaLFixclaNzIsaPHjyBDihy5MYLJkyhTqlzJ
+smUECDBjypxJs6bNmzhzxnzAs6fPn0CDCh36wIHRo0iTKjXaoKnTp1CjNmVAtarVq1ipLtjKtavX
+r1sViB1LtqzZs2jTKkjAtq3bt3DZIphLt67du3MP6N3Lt69fvQYCCx5MuHDgAogTK17MGDGBx5Aj
+S578eIDly5gza7YsoLPnz6BDdw5AurTp06hJA1jNurXr16sDAgA7
+__theme/sonia/css/plavo.gif__
+R0lGODlhEQA8AKIAAOTu+ebv+efw+enx+ury+uzz+u30++/1+yH5BAkAAAwALAAAAAARADwAAAjC
+AAEIHEjwgMGDBwgqRHhQYUGGCR0KhBhRIkWJEyFiBHARo4GPIA1sDAlyJEmRHk9uBKDSZMiVLVO+
+dGmgQIGVNnPe3KjTJs6eOyUCDepwKE+gR3sm1bk050YCUKMSeCoVKtWqV6WurDo1q1WvXTFy3Rp1
+wICVZtOe3ajWLNq2ayXCjetwLlu4d9vmVbtRgN+/AvoC9it4cGHAGwMoXhxgJWPFjh9HZjx5cWXI
+iSVnpryZs8THmD+DXkm6tOnTqFObDggAOw==
+__theme/sonia/css/datum.gif__
+R0lGODlhBQAVALMAAN3d3evr6+3t7fDw8PLy8vT09Pb29vn5+fv7+/39/f///wAAAAAAAAAAAAAA
+AAAAACwAAAAABQAVAAAIMAAVCBxIsKDBgwgTKkzAkCGChw8PSJRooGLFAhgxEti4cYBHjwJChgxA
+kiSAkycDAgA7
+__theme/sonia/css/lijevo.gif__
+R0lGODlhEQA8AKIAAO7u7u/v7/Dw8PHx8fLy8vPz8/T09PX19SwAAAAAEQA8AAAIwgABCBxI8IDB
+gwcIKkR4UGFBhgkdCoQYUSJFiRMhYgRwEaOBjyANbAwJciRJkR5PbgSg0mTIlS1TvnRpoECBlTZz
+3tyo0ybOnjslAg3qcChPoEd7JtW5NOdGAlCjEngqFSrVqlelrqw6NatVr10xct0adcCAlWbTnt2o
+1izatmslwo3rcC5buHfb5lW7UYDfvwL6AvYreHBhwBsDKF4cYCVjxY4fR2Y8eXFlyIklZ6a8mbPE
+x5g/g15JurTp06hTmw4IADs=
+__theme/sonia/css/header.gif__
+R0lGODlhEABGAPcAAHJ9iHN/ihJ0xHaCjnmGknuIlH2Kln+MmX+OmoCGjIGHjYKIjoSLkYeNk4iO
+lICOm4qQl4KRnYSSn4ySmY2Umo+WnZGXnZKYnoWUoYaVooiWpIqZpoubqI2dqo+erJKZoJSboZac
+o5eepJifppCfrZqgp5Ggr5uiqJyjqp2kq5+mrZKisZSjsZSks5amtZeot5mot5mquZusu52uvaCn
+rqGor6KpsKOqsaOrsqSrs6Wss6attaeutaivt6GvvqGwv6mwuKqxuauyuauzuqy0u621vK61va62
+vrC3v56wwKGxwaSywqO1xKW1xqi3yKa4yaq5yqu7zK29zq6+zq+/z7G4wLK6wbK6wrO6w7S7xLW8
+xLa+xbe/x7e/yK6/0LnByLrByrrCy7vDzLzEzb3Fzr7Gz6/A0bHB0rLD1LPE1rXF1r/H0LbH2LfH
+2bfI2bjJ27nK3LrL3LvM3rzN3rzO373P4L7O4b7Q4b/R4rzW68DJ0cHK08LK08LL1MPM1cTN1sbO
+18fP2MfQ2cjR2cjR2snS28rS3MrT3MvU3czV3s3W38DR48HS5cPV587X4M/Y4cTX6cbX6MbY6sbZ
+68fZ7Mfa7cja68ja7crb7cnb7snc78vc7szd79DZ4tHa49Hb5NLa5NLb5NLc5dPc5tPd5tTd59Te
+59Xf6Nbf6cbe8srd8Mve8cjf8snf8sze8M3f8czf8tfg6tjh69ni7Nnj7drj7drk7tvk7tvl78rg
+883g88zh887g8s/h883i887h9M/j9NDi9NDj9NHj9dHk9dLk9dLl9dTm9dXm9dbm99zm8N3n8dfo
+9t7o8t/p89jo9tnp9tvp9trq99vq993q+N3r+N7s+ODq9OHr9eLs9uPt9+Dt+OHt+OPu+ePv+eLt
++uTu+OTv+eXv+eTv+uXv+uXw+ufw+ufx+ujx+uny+uny++ry+uvz++30++71+ujx/Ory/Ovz/Ozz
+/O70/O/1/PD2/PL3/PP4/fX5/fj7/gAAAAAAAAAAAAAAAAAAAAAAACwAAAAAEABGAAAI/gAFCBxI
+sKDBgwgTDmzBgweLhisg7tixQocOE1u2KNGiJQlHjx2zZEmSxY8XPyb79PGicgofPlL46LEzKlSd
+UDZBgaKjk44nT3KWLaMkdJIyZUaVQcKFayk2VdieRtV07RpVq1VhadO2Klu2rl+9dn0KDlywsmfN
+lu21la3at2njlh0mThxdu3Xv6q1bjK9fcX0BizOXzBxhw4UPK07MGLHjxY8bH0YXDR1ly5Uva87M
+GbNnzdQsh0Y3ujS6dNXSoVaderXr1rBZyzaX+ptq2+lw677Njt233r99Ax8uvHjw473JJV/OTnnz
+3ubatYs+XTr169LRZd/eTnt37t7D/nNP185dOnfm0aNDf978vHnw5tGLRw/+e3j05s+T907efHr8
+ycNffvEI2Ft+9PSmIIIKslPPgw06CKGCD9ajzoUXVojhhfbYs86HH3boIYjrdKjaiSKeqFqHlrUo
+YouWiXjOjOfcYyONM9p4zznl9Khjj0D+WM444+hI5JFGEumNjt402SSTTuIjZThUUiklPlVaiU83
+XHJ5ZZde5pNlOPmUOWaZTjZZZj5perMmN3By82accpbJzTZ4ronnnnpuY401a/4paKB/VrNmNYgi
+emiia07jqKONPjpNmdJUWumall6aDzSccrpmp57m88yoo65JaqllMqMqM2uuqmqreMjEumastM6K
+zDHHrInrrrriasyaxgQbLLDCrknMsccaiywxZQrjrLNrPgttPsBUW+2a1l6bjy/ccrtmt96Wucu4
+u6xJ7rjm5qLumuq2y24urbSyZrz0zhsvK2uyoq+++e67ZioAA/xvwKnkYfDBCCes8MIMN3xwQAA7
+__theme/sonia/css/bg.gif__
+R0lGODlh+AJYAsQAAODg4OLi4uPj4+Tk5OXl5ebm5ufn5+jo6Onp6erq6uvr6+zs7O3t7e7u7u/v
+7/Dw8PHx8fLy8vPz8/T09PX19fb29vf39/j4+Pn5+fr6+vv7+/z8/P39/f7+/v///wAAACwAAAAA
++AJYAgAI/gA9CBxIsKDBgwgTKlzIsKHDhxAjSpxIsaLFixgzatzIsaPHjyBDihxJsqTJkyhTqlzJ
+sqXLlzBjypxJs6bNmzhz6tzJs6fPn0CDCh1KtKjRo0iTKl3KtKnTp1CjSp1KtarVq1izat3KtavX
+rwc7iOVAtqzZs2jTql3Ltq3bt3Djyp1Lt67du3jz6t3Lt6/fv4ADCx5MuLDhw4fFdmDYgYOGDBkw
+SJ5MubLly5gza97MubPnz6BDix5NurTp06hTq17NurXr17Bjy55Nu3ZtyBo4LEbYQQMGCxQmSBhO
+vLjx48iTK1/OvLnz59CjS59Ovbr169iza9/Ovbv37+DD/osfT768efMTKFjAoGG3QQ4YKkh40ICB
+/fv48+vfz7+///8ABijggAQWaOCBCCao4IIMNujggxBGKOGEFFZo4YUYZphhAw9IUAEGHISlgQUS
+OKDAAQWkqOKKLLbo4oswxijjjDTWaOONOOao44489ujjj0AGKeSQRBZp5JFIJqnkkkwyeYACDkhg
+QXsGdZABBQ8oQEAAAHTp5ZdghinmmGSWaeaZaKap5ppstunmm3DGKeecdNZp55145qnnnnz26eef
+gAIaAAEKPEBBBu4NZOUEDRwQAFiQRirppJRWGlMABzQwAaLvYSABAwUAYOmopJZq6qmoAlAAAxKA
+WKWn/6CKiuqstNZq661IqcoqBokK1AGsBsiK67DEFmvssSMBYMCuvXrw66ehIivttNRWa60Hurba
+LHzQCnvtt+CGK65X2bpaELexjqvuuuy2m+uqrYZ4LqzRumvvvfjm61K58hKEbr36BizwwARTxG+n
+3Ras8MIMK3zwvAk3LPHEFIv7sL/0elvxxhx3XOvFA/2rscckl2zyVyALJPLJLLfsMlUpe7DyyzTX
+bPNQMc9888489yxTzhn7LPTQRJMEdMRFJ6300hIdnS7TUEct9UBOAzz11Vj7XPXIWXft9clbfy32
+2CSHTfbZaDNsdtpst43v2m7HLXe4cM9t993H1o333v58z6p334AHPunfghduuFaEH6744lAlzvjj
+kBvleOSUV87T5JZnrvnP8JqLMdKbhy56TJiPbvrpH5WO+uqsV6R667DHvtDrstduO7ad9xty0Lf3
+7rtBtP8uvOjBD2+85cUfr/zjyS/vvOHNPy9939FPb73d1V+vfdvZb+892d1/L37X4Y9vvtTln6++
+0umv777Q7b8v/83xz2+/y/Xfr3/J+e/v/8b9+58AGxbAARqQYAU8oALzlcAFOrBdDXygBC2WO4Q9
+bYIY5F8FIXbBDHoQgBv8XAc/SEIChnB3oCuhCgsWwRW60G8nVBnvXkhDBsZQZjOsoQ4heEOd7fCH
+4/5qIRCH2BUhEvGIWDEiEpc4FSUy8YlOcSIUp5gUKVLxikSxIha3+BMtcvGLOvEiGMdYEzGS8Yww
+MSMa17gSNbLxjSZxIxznGBI50vGOHLEjHvd4ET3y8Y9N62EOAUnILgoyhYVMZBgPOUJFOtImfnyk
+JHG3K93JEJGTzGRLIqnJRHKyk4T8JCj/KMpR7rGUprwjKlM5x1Wy8o2ufOUaYynLM9KylmO8JS6/
+qMtdbrGXvrwiMIM5xWES84nGPOYSk6nMIzKzmUN8JjR/KM1p6rCa1qQhNrPpwm1yU4Xe/CYJwylO
+D5KznBg8JzolqM51OrCd7lQgPONpwHnSU4D2vP+n//KpT/3xs5/2+ydA5SfQgbqvoAZVH0ITar6F
+MlR8Dn2o9yIqUe1RtKLWi9mzGolRTZarWRsNVkdTqSxmvQqTI5XkRy1otZRm8qIuVR5MY2q8mdJU
+eDa9qe9yqtPb8bSntfspUGMn1KG2rqhGXR1Sk3q6pTJ1dBod5FPxuNKChJRrU23lsrR1Uo5mFY5V
+FWFLv9pKRo6VrLA0K1bRikanspVybn0r5OIqV8bRta6KuyteoafWvbJRr34NHGADS72+EpaMgz0s
+3hKrWOwZtrFcjOqnRArZyG6VV12lbGWxWFKuWhVYa90sEjuLWQ6eVbTLfCxqocjY1aKtta4Fn2r+
+Yzva2dKWiLC9rddyq9us8ba3V/stcNFn2+HWULjGZRpyk8u+4jJ3hct9LtGiK134Obe6H6Qudnmm
+3e3S77renWB3w0uz8ZIXf+A97wLNq16wpbe99XwvfPEp3/nus7729Sd+8xvQ/fKXoP7970EDLGCF
+ErjADT0wgiGq4AVPtMEOtiiEI5zRCVNYeuy9MAstrOHlZbjDAvswiPUl4hG/jcMmximKU7zTFbPY
+py5+cVBjLGOi0rjGR70xjpWq4x03tcc+hiqQgxy6EhP5W0Y+srWSrGRqMbnJ0noylPM25ClHTspW
+JhaWs4yrLXPZVl7+Mq3CLOZUVbnMfK0kS0P+i+YZq9m0bG6zjd8s1jjLOcd0RqFX7yw7MvO5Un7+
+8+DOLOjFErrQjs3zJfeMaNQFutEoOzSk3fboSXOl0pZGnKQz/dpNc1q2isYhSj+9OUyTuiqmPnUT
+Pa1q8rG61VhLNayfIutZN6XWtl4KrnNdxVfzWrm+/nVzQ+1DYc812MaeLrKTbV1iS5XZh9s1tIMi
+7Wkb0tmjtnZhsc1obe+t2t7eCbjDnZNxk/sm5j53GZet7palu92c4/Zp4S23d9P7Jfa+9ybZrW+P
+5bvfKvk3wFEi8IHHkd8Gp1jBE54shDPchPK288Nj7fCJO6ziFkcgxjMe4o1znMQe//iJIy7/clDH
+a80lH9vCU46RlbPcIi5/ucFCLnN1xbzmEbk5zh+i8503RLIM0KzPoUZakIJ26Oi7rNEnK3Gk00/p
+VcrABByAAAE4HWoCQIADNgVSqVPd6ldfWta3zimrXunrYRe71g/VdQqgPe1JGzvbo+72qsM97msv
+O0GsVPcB3L1oA8h726nu978PLfAOmLvZ+274wwue7oRvvNAQr/i9nx0BhZc8zyivd0Vd6QGY13zP
+Am+ozvvq86EX/c5IX3nPYykBmVd9zQaQgNK3/QGwl/3NaG97uuM+9rp3Ge9bf/rXEyD4NSNA7Ynv
+rM8n4PjIf5nye7943EM/+i2bPvP5bn3s/rtM+6ZvvvG9n/3lh5/7zyc/y8B/+/Sr32Ts973730+y
++Fd//vTvmP0tP/78e2z/rpclBeB/HlMAhbJ9n6cAA0iAHGOA1Ld3GlABEKCADNiACgABFUAlVhWB
+E7iAFUgxBoiBGgiBEkiBHwiCF5iBIMWBJniCEhOCKlglLOiBLsgwMDiCijKDNfiCKYiDvqKDO2iD
+PbiCJUiDQUgwN0iEHXiEC5OEMliCBsCECmMAQ/iEELAAUSiFBGMACyCCSoiFWriFXRiDGyiBYBiG
+AsOFXmiFZ4iG+qKGZEiCV5iFbpgvcOiDzsKBbViH93KHX0iHfGgvfviEEbCHgdguXBgB/3GYgxVQ
+iAdwiPdyAAugiHjYG424AI8Iie4iiZRIhI6oiZs4iYv4g5eYiaC4Lpw4innYiAxgiqc4LgfAAJ1I
+iK34iqgoi6poiRFQi7YIi7iIhx4Qgbvoir34LbFIiQgxIhHQKMUoLpkSAVOSjBawjMTYjNXyjNF4
+EMrIAAhgjeCCALKYjRs4jdzojd8CjtBYidvYjeZoLeg4JStIjuzYjtTyjuooj/RYLfYYj7s4j/mI
+LPsogyRSjv8oLeAoJff4Kf5YkMVykPAokBLQAAvJkMOCAA2AkPEYkRNJkbdikRgJkQ2QABxpLAlw
+kQ85jhEpkiNJLCX5kQYxIim5kixpkv8aII0lopIyeSsJECXiWBAwGZI5iSst2ZM5SCJAGZS2MpQJ
+eZRISStKmZFM2ZSo8pQgiZNSeSpUiZJReZWlkpUQSCImwpWoAiUu+ZUlogBieSpkeZJmGZZpWSpr
+WYkbcAFTh5ZvSSpQMgEXsAEgNZd1eZd4uXV72Zd06ZaAWSl5OZhV4peGeZiTkph8uZiFaZeO+ZiC
+GZlWxZiUWZmRApmE+ZecKSmeKZmgGZqQMpqZWZgLYJqRsgCX+ZkPsJqsCRYL8AB6iZl755exOZu0
+aZuKmZoTsJu86RW1eZuwKZvDyRXF+Zu5SZfCmZxbsZy4qSi6iZzQmRXSeZzXGZ2+OZ3/vlKd26kV
+2SmZWMIA4ZkVDGAozEmdF1Ce54kV6UkB6/md7fkA5vmeVhGf8+ksc+me+FkV+umd/NmeEHCf/zkV
+DAAB8imgHdCfBXqgVJGgC0qYFPCgECoVErqfDUqgBnqhT5GhDOqgHeqhTQGifYkBFTqiJLoUEooB
+IYqiENAAK/oUDaCgLnqiFSqjM9oUNUoBN7qYMKqjO7oUPfqjmRmkQ8oURfqiOZqkRGqjTBqjTqoU
+S4qjyzilSdEAEeCjL9qIQoqlRaGlH9KlVwqmRiGmRpqb8VGmZkoUaEqmDtCmReEAipim1LmmcSqn
+Q0GnY3qijZinehoUfGqn34mngSoU/4MKp4cqqHWqqIv6E4nqpxEAqI/KE5EKpH9aqT5xqUcqH5Sq
+qTkRJX2KqSUCqpbqIYTKn/ExH6a6Ex0yqp3Kqq2aE6+aqg26qg8wq7SKql0qq7pqE7Xaq7n6q8DK
+q80iMxlAIsNKrDTRIRaQAZYUMsnqq8wqE84KrQjBAdO6rNUaE9carSqzrd06E9+areI6rt4qJdh6
+ENqqrOiars8KrshKIhDwrjABAeoqr+06AfVqry4BARMQr+ZqAfzqr/8asOv6HslasAbLEgArsOy6
+sP3asCrxsAl7LhJLsSthsfqasRqbEhw7sAz7sScRshFLsBNLsiVhsgpLsBGgsicRAf8I27EuC7Mm
+IbMQ27IT8LI2SxI4e7H+kgF0ybM9KxIyewFAK63tSbRFCxJbirQdu7RNa7TymbThKrVT67RVG7UU
+wLRZyxFPa7XIirVf2xFhy7VeW7YZcbbm2p4SoLYdIQFbm60a4LZwyxFyewG5Qbd2e7cakbd7y651
+SwFv67cZAbj6OriFa7gXgbh8S7iMixGOK7gXIB+R27gVoLeJW7mLe7kT4SGay7eW67kUAbqB+x51
+O7qkKxGmm7jxMQGrOxET8CGney6+UQGwG7sRMbvs4bq4q7u7S7u+m7vA6xC8W7v+crvEW7wMcbzD
+y7zGK7x0+7rQ2xDOO72/W70Lcb3/gvsby6u9BxGwvTu9BAu+CiG+yBsyvgEc5psQ6jG+3cu+7XsQ
+75u+KrO+FDC/9Lse9isz+Ku/BlG/riu/ADwQAky++VvABsy/A5zACuwBBxwWG5CsDqzA6pEBAvqd
+FPzAAnHBGcyfG8zBHnysIEzAFvysH9ygQlsBHOwBmYvBJKzCldvCL5zCEzzDHFzDMXzDLJzDSGvD
+K0zDP7zDQezDMMwbPCzERyzBRfzAOozETazAT8zEF2ABLWwBQwzFVXzFWUzFVszBWLzEixkZX/zA
+6yHGmbm+V8weNqzGYMzGO+zGZgzHSCzHCsy/bfwba6wBeVzGd0zHEuwbF9DCFwDI/4spyIRsyGmM
+AYPMwYXMxzHsGIycyLkRyYjsyL1ryZOMyZXMG44htIScAZ0cFp/cyA+MtKNcJaUcyqlsVavsyKKs
+G578GBjQwhgQy5E8wbXMwbe8AbJMyrpsyxj8y6oczLw8zLkcGcLsy8m8yw/cy8Tsysb8zMjsydOs
+wNDczMsczXvHARvgG7bMx9ysKN78GC0sysxszebMweg8zr5SzhlwzuKcy+v8wO1Mz/HMzvOszvls
+z/sMzPWswPfMz/KczrPczwLdyqoc0AWMywrRGAwNwA6dEI3xzS38z+p80Qad0RyM0QCt0e5Mzhbd
+0Rv90SQd0u880g/s0cVckydNwvYp7dIrXdLFvAEtvAE0Lc02zcE4jdLO4s07/cA9DdM/7c03TRZE
+3RhGzdNI/dAVfdRi4dRLLdS6kdRPzdRRTdFXTdVZzRtbrcDMbNVk0cJILdby2r5lvRBKTdY+7StK
+jRhwHddyPdd0Xdd2fdd4ndd6vdd83dd+DRdd7dRv/deEXdiGfdiIndiKvdiM3diOnReK8RCKMdmU
+XdmWfdmYndmavdmc3dme/dmgHdqiPdqkXdqmfdqondqqvdqs3dqu/dqwHduyPdu0XduV3cK4ndu6
+vdu83du+/dvAHdzCPdzEXdzGfdzIndzKvdzM3dzO/dzQHd0BExAAOw==
+__theme/sonia/template/tt2/kwiki_screen.html__
+
+[%- INCLUDE kwiki_doctype.html %]
+[% INCLUDE kwiki_begin.html %]
+
+  <div id="bg">
+    <div id="sadrzaj">
+     <div id="toplinks"><a href="mailto:">Contacts</a></div>
+     <div id="zaglavlje">
+	<div id="title"><font size="-1">Perl-Hackers Wikis &gt; [% wiki_id %]</font><br/>
+                        [% screen_title || self.class_title %]</div>
+     </div>
+
+    <div id="navigacija">
+     <div id="menu">
+     [% hub.toolbar.html %]
+     </div>
+     <div class="lijevo">
+       &nbsp;
+       <div id="widgets_pane">
+         [% hub.widgets.html %]
+       </div>
+       <div id="status_pane">
+         [% hub.status.html %]
+       </div>
+     </div>
+    </div> <!-- navigacija -->
+
+
+
+
+   <div id="clanci">
+       [% IF hub.have_plugin('user_name') %]
+       [% INCLUDE user_name_title.html %]
+       [% END %]
+
+    [% INCLUDE $content_pane %]
+   <br/>
+   <br/>
+   </div>
+ </div><!-- background -->
+
+[% INCLUDE kwiki_end.html -%]
+
+__theme/sonia/css/kwiki.css__
+
+body {
+  margin: 0 auto;
+  padding: 0;
+  font: 9pt Arial, Verdana, Sans-Serif;
+  background: #fff;
+}
+
+a {
+	text-decoration: none;
+	color: #2489DB;
+}
+
+a:hover {
+	color: #808080;
+}
+
+a img {
+	border: 0;
+}
+
+h3 {
+	font-size: 1.3em;
+	margin: 0 0 5px 0;
+	padding: 2px 0px 0px 0px;
+}
+
+#bg {
+	width: 760px;
+	margin: 0 auto;
+	padding: 5px 10px 10px 10px;
+	background: url(bg.gif) top left no-repeat;
+}
+input{
+    border: solid 1px #2489DB;
+    }
+textarea {
+    border: solid 1px #2489DB;
+    padding: 3px;
+    width: 100%;
+}
+
+input[type=submit] {
+ cursor: pointer;
+ padding: 3px;
+ border: none;
+  background: #2489DB;
+ color: #fff;
+}
+
+input[type=submit]:hover {
+ cursor: pointer;
+  background-color: #A9CAEB;
+  color: #286ea0;
+}
+
+#sadrzaj {
+	padding-top: 9px;
+	color: #303030;
+	width: 736px;
+	padding-left: 2px;
+}
+
+#toplinks {
+	height: 20px;
+	background: #2489DB;
+	text-align: right;
+	padding-right: 10px;
+	padding-top: 5px;
+}
+
+#toplinks a {
+	font-size: 8pt;
+	display: inline;
+	padding-left: 5px;
+	padding-right: 5px;
+	padding-bottom: 3px;
+	margin-right: 5px;
+	color: #fff;
+}
+
+#toplinks a:hover {
+	border-bottom: 2px solid #fff;
+}
+
+#zaglavlje {
+	clear: both;
+	margin: 0px 0 5px 0;
+	height: 70px;
+	background: url(header.gif) repeat-x;
+}
+
+#title {
+  color:  #2489DB;
+  float: left;
+  font-size: 16pt;
+  font-weight: bold;
+  padding-top: 10px;
+  padding-left: 5px;
+}
+
+#title_info {
+	float: right;
+	height: auto;
+	padding: 8px;
+	width: 310px;
+	margin: 10px 5px 0 0;
+	border: 2px solid #fff;	
+	background: #FFF5D9;
+	color: #808080;
+}
+
+#title_info p {
+	margin: 0;
+}
+
+#navigacija {
+	clear: left;
+	float: left;
+	width: 150px;
+	margin: 0 0 10px 0;
+	padding: 0;
+	font-size: 0.9em;
+}
+
+#navigacija #menu {
+  width: 150px;
+  margin: 0 0 3px 0;
+  padding: 0;
+  font-size: 0px;
+}
+
+#navigacija #menu a {
+  font-size: 13px;
+  display: block;
+  margin: 0px;
+  margin-bottom: 4px;
+}
+
+#navigacija #menu a, #navigacija #menu a:visited {
+  text-align: right;
+  height: 20px;
+  text-decoration: none;
+  color: #fff;
+  display: block;
+  padding: 6px 8px 0 10px;
+  background: #2489DB;
+  font-size: 13px;
+}
+
+#navigacija #menu a:hover {
+  background: #A9CAEB;
+  color: #286ea0;
+  font-size: 13px;
+}
+
+.lijevo {
+  margin: 2px 0 10px 0;
+  padding: 3px 10px 5px 10px;
+  width: 130px;
+  border-top: 1px solid #DDD;
+  border-bottom: 1px solid #DDD;
+  color: #6B6B6B;
+  background: #eee url(lijevo.gif) repeat-x;
+}
+
+.lijevo a, .lijevo a:visited {
+	text-decoration: underline;
+}
+
+#desna_rubrika {
+	float: right;
+	width: 223px;
+	margin: 0 0 10px 0;
+	padding: 5px 13px 20px 10px;
+	border-top: 1px solid #DDD;
+	border-bottom: 1px solid #DDD;
+	background: #eee url(lijevo.gif) repeat-x;
+	color: #5D5D5D;
+	line-height: 1.5em;
+}
+
+#desna_rubrika p {
+	margin: 0px;
+	font-size: 90%;
+}
+
+#desna_rubrika h3 {
+	font-size: 9pt;	
+}
+
+#desna_rubrika a {
+	display: block;
+	height: 12px;
+	padding-bottom: 4px;
+	margin-bottom: 2px;
+	border-bottom: 1px solid #ccc;
+}
+
+#podaci {
+	margin: 15px 0 0 0;
+	border: 2px solid #fff;
+	background: #E5EFF9 url(plavo.gif);
+	padding: 3px 3px 3px 6px;
+	color: #70C256;
+}
+
+#podaci a, #podaci a:visited {
+	display: inline;
+	border: none;	
+}
+
+#clanci {
+	margin:  0 0 5px 155px;
+	padding: 0 10px 5px 6px;
+	line-height: 1.4em;
+	background: url(clanak.gif) repeat-x bottom left;
+}
+
+#clanci h2 {
+  color:  #2489DB;
+  margin: 0px;
+  padding: 4px 0 4px 0;
+  font-size: 12pt;
+}
+
+#clanci p {
+	margin: 0;
+}
+
+p.new { 
+  text-align: justify;
+  text-indent: 25px;
+}
+
+.datum {
+  text-align: right;
+  padding-top: 2px;
+  height: 30px;
+  margin: 5px 0 6px 0;
+  border-top: 1px solid #eee;
+  font-size: 80%;
+  text-transform: uppercase;	
+  color: #808080;
+}
+
+#lijeva_rubrika {
+	margin: 0 250px 20px 155px;
+	border-top: 1px solid #AFCEED;
+	border-bottom: 1px solid #AFCEED;
+	background: #E5EFF9 url(plavo.gif) repeat-x;
+	padding: 5px 6px 5px 6px;
+	line-height: 1.6em;
+	text-align: left;
+	color: #5D5D5D;
+}
+
+#lijeva_rubrika a, #lijeva_rubrika a:visited {
+	text-decoration: underline;	
+}
+
+#lijeva_rubrika h2 {
+	font-size: 1.5em;
+	margin: 0 0 0.5em 0;
+	color: #808080;
+	padding-top: 3px;
+}
+
+#lijeva_rubrika p {
+	padding: 0;
+	margin: 0;
+}
