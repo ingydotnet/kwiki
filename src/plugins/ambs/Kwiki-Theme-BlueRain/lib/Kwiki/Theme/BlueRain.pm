7c298d317f015fe5c0f9e3b6fac597d212482f60
ambs <ambs@4b3b33bc-d223-0410-9309-b363ae52eedd>
2007-01-27 15:57:07 +0000

    Added theme
    git-svn-id: http://svn.kwiki.org/kwiki/trunk@101 4b3b33bc-d223-0410-9309-b363ae52eedd

---
+++ src/plugins/ambs/Kwiki-Theme-BlueRain/lib/Kwiki/Theme/BlueRain.pm
@@ -0,0 +1,297 @@
+package Kwiki::Theme::BlueRain;
+use Kwiki::Theme -Base;
+our $VERSION = '0.01';
+
+const theme_id => 'bluerain';
+const class_title => 'Blue Rain Theme';
+
+__DATA__
+
+__theme/bluerain/css/header.png__
+iVBORw0KGgoAAAANSUhEUgAAAEcAAABsCAMAAAAPBV/YAAAABGdBTUEAAK/INwWK6QAAABl0RVh0
+U29mdHdhcmUAQWRvYmUgSW1hZ2VSZWFkeXHJZTwAAAMAUExURWqS2aS52TltzGiIuz10xjRkukF8
+1LjK5i1Ykz53yi5ZoklyuD110a/C3q3C4jltxKzE7DFfnzhrtjBcmjFfoFR4tS5ZlT540TZosTls
+0TNjpzxzw4miyzdoujhsuLbJ42KN17vQ8UB61UB60j941TxzxDJho1qDw2WIxzJhqTBdnLbH4S9b
+mbnM6aK74ytUjDJgpTFeqpSu2jRlqnSb2z94z3me3DxyzpWz5DpwwLTG4Jas0D53zIul0Zu03Tpv
+vj111I6r3DRjp1iF1DVlsTxy0UJ+2puz1kRqqYmq46O+6YKi2jpwzjRkqDBdpbrP7oOeyzVmrEt9
+0rLJ7jFesjdownuh4TpvvDZosjVmtEB71j11x0N00jluujpwxT94zYCk4nORwzVmrkRyzDdoyz52
+z0VvsThsvDVlwbXK6FB+1DJhtnGTynqc01OD0rHG5Tdqtjxzykt6y5m25UB60DRkrDtxybTK7Tpu
+v7bM7jhsujZmrjRiqo+p0zNgsLnO7KbA65Gpzjpv07rO7ThqubnO7j10zER5z4Wn4jRjtTJgrZ64
+4yxWnn+h3bjO8DhstzZotYKg0z9orTVjsTxzz7HF4Vd/wTBaozhsvjxy1UB40jJgp7PJ6jRkrzdq
+tDx0yjRirkJ82Dlkq1+I0zZotzBap1qJ2jlvvXmWxqm+3z1qtTtx1K/H67nN7D12y3ee4EF82DZn
+yUB613yazZSz6EyA1DposjdqvD50zEF+2UJwykd40jtywztxwDluvm2QyS5Yn5iv03SX0jdnxTVo
+tD51yzNjrC1XkSxVjSxVjjJgoSxWj0J+2C9blzFenTNipS1Xki9cmixWkC1XkC5ali9al0J+1zx0
+xT52yUN/2jdps0J/2TtywTZor0F91rrQ77jL50J92T12zjpuuzNjqT11zTltvT11yDdlq4un2KjA
+5zFfpDdksD1ruj5rsD9vuW+W3Z+76TtsupCw5pCu4jltv7fL6zJZoD1jqHSXzlOD2FqF2WCBuUBz
+x055vkl0xqfUjf4AAAlqSURBVHjazJF7XFPnGcdfopABERiTBRAIJqCyLU4QiMFwE1JvyEEuKigX
+KSFAvCRFoigFReQiCqgICqK2SFsqN+0s1QbUWdjNzV0rY2IiGlih67Bqa1dn97znJBAgWNnn/OE3
+n4+e53l/z/c85wX9nB7Qr+nhlfP8hh5o8/yOHmjz/Jke0F/oAf2KHmjz/JEe0A/o4ZXz/JgeXjnP
+X+kB/Y0e0O/pAf2MHmjz/Ike0I/oAf2EHl45zx/ogTbPD+kBzaYH9Ft6oM0zjx5eOc8tevgezyID
+XuxxnArKMjwcPht+wwBpmiqNlhkHS27ZbY64btLo5zfLZLnTZrunoMMqo3m0xgjLHG/NOzpj3yw/
+v58a4Hd984rZYHJcZmTEmMfRcZGdP6XwM1m6zynCaamJrlz6bHb4olojJrR4ImsezKu6joeO75ux
+4utDOq6teOY0ixTPGB6udVwzcWqyJyd1H7YsfQaOaxYW1TosLK59beeE11puF167bJJn7TgWL3bc
+fByyTivAUR0eHl5b60FSWwt/uWqLp+/BqV/ErdqcrMXjBid4sv7pxIFdVhyysACHR86D7iwd3Q9y
+PGrDw6ufRsBrrr89nJOVNc6TbMDarC+Wcziznh2CVWo9unHS4BDe0g0qCzsTDscktfZBlsFhMuob
+I3ltKkSWP4VdPHMgtadvAnuSk7NyPMJznOBllvBpyWNHyDCV2sjh+FdbVNdiS59RKNN7HM5xO49u
+gxCy19OX/AVss6/aIvxgd3Jyn/1UgMnDE0SzUj2ykpP1XdRrr/v1fQh382ZDtacHPux9wc8+uRuL
+Go961Nn36ZqoV4f9Hn/4qIYGz7OhYHkhIDqLRcvfPhiqj6LTFL32W+AANN2hvb2nv49e+7qDEfBa
+zzp7XVjv2WDpx7E6ird5CQ0p+hxus+pg3QYqjmxJTtt7MTXPGjxx2/ZlOL2hzhKu6GzdBipPeeTy
++Rzm9Ybo3Fi53PblkMce+4DJ3JwbWkFOoHKM7YYAJjPVEzS25VQD1FhZPgbV0gfKQfShFdPq82Of
+yHGJXDDyKibTvyE6tMKWLF1s5V9WzbeM/QRMLi76lvz5/Ofwdrk+UxG6hcm8it8NFekpl7/JZFpG
+H6uwLXeh6i3Hmcz0N9+vkJfrNOW2VY1MJjPAEq+sF8FCjbmhZAbFAy7/5jC9GsDsEk+WLlc1TIx/
+SYWuBZHjZMvq/djRljwWbuh5bgWuKQ/MLYguqdAHXBo1JByDoW+oFnPLaCze5dxzjeZIbqyLC+XZ
+XeCl2X4sOrZgN3W+uyCdSU1Zys7pegVHqI4mQlZREK/PxZYyraJL5FLwrF+/Pv7L7Rqv6JJzu++v
+J4m3LaVmtueWOMeTrfvtV3X7LJCN5tbHF3ygYVqWFEAGBQYGSudrNFejS5wHAinu735uhWfSF0TH
+tlPN+1LYGeMPOen90WCVRrOAbGDPwDcaPnyB9F3d8bsDBf/a4hVwxBLP6Jqd7QULggP8q6Jl59o7
+A8eC6ZqtMhxCQmGg9DONpsQU1hHq2CV1NpXJomWmzlJ9M3Cg3VmGcW4fywkHnBdqgsETKEQdHUJp
+AL9RZtq+q0MPTEnbnZ1hZJdwtNdJtaRjrY6OQGmwphQ8wg6Ei0Z+gMxUOubpgJU6BwYGdgUazFCt
+TsMWjB7RwArSwA50547wxHa+l8z0hPCOAVTuzp1JvfGdga387eQoUqmEJ9L5n8mcnwhV06aj85f8
+dFnaCaEKzZmjegIeU+cnqjnTRiUEDzmK3N1VT+C7TNOeqNynjUoI30WOotu3VbuGmgNMna1Vt6eN
+Sriab0OOopkz3a0X8hemxVi7z5w27gnB/IWmeJT0BPP5/6cHVnhMeTZuvJ2wtbn5OygGN04X94RT
+zavTYhJub0SDgzMT6pv5rjHWysFpo9rWzEdp1gkzB1FPz6Dyv1/lBYNnY880GVRuzct7Iw1PgqdH
+kVCa13QjJuHM9D2ZeafSblgrunqQubk5qa2PUSp6zKfH4BvNeY/TYpRnzM2xp0UBdTBoW6anaTnj
+nddcf8NaCXNIJBK1nIH9vrpirezCRUtXYVdLi2hKWjDUk2II7uMGQwElSgRalGj//sM3lIWixESR
+r3eXohCMiUaB6cgibQs+FXWh/c2H4Za7oCA9okLl0P6dV0CcKGpZtX+oXjmVSdTV5b0zU6ElPR8N
+wVdcYChwgXww/QpE5K1MUWpht6LiZiIzRKnQ9icm+ownUaTdVko01Su1Iij6vQn16jSGsh/HKI+P
+VrCKyNvGUEh8RFplxhKCyHQVKAq1EpGBRSTRXlrZTDx+nSHAHlFkFHHqSgpDQGbQDoyPjyDkADF0
+JUzr4yMpFDBClhwgbJJCKgsL+yUSCcTg3/4i1yV5xFA9gxEmIFuZBIEuMOAZGyjPDp9+QTFBPGYI
++iGgFYQxMopPEYTNY2/XbZFFkqLIEFffJdkE8dCVARpFP9b4EkTphRSBltTsQDwStk/lR+A/zKBe
+VQmmsBDfzJ3EGE2PRiKxRVDJhggbwcavpzCgIAU6D5gqi2xAFFbJxgWbVSnArsj6EW/fdb7eKORj
+7AAJi00GkJrIzrjAINPjPDy2ICOKmOtLicDUj1WCsFGgqNRZQJNNHKiHO5aMesx08HgScQhcwTqG
+uIbHw3VNDVvCYrEqKeBJUgMnPHwyUgZ3nJIiZtXwdOOjHjMujyX4NkqtXrmJxebxqA6mxo0N1JDP
+XPKFvNYydRlKYYglPL3GwAMBljijSa3emyGWuI0mKBmPyx0tL2Wq1dn1KQ5hrDGNoccsjhsk/vih
+Wl1WvIkV5MaNM5tEHNfNrXWnWm2TkeIgZrlxx07QXQPMuGzxpkdqtbqptYbFxrFxp2ZcLndkLxw/
+3OTgIGZzDU/Hee7C+4LyXW0gGpX0n6AgN1hKt1UcXuVSK3k04uCQL3Ybp5ngAZNbkFjcGjUXWJX0
+ThB26Xhn3cMy6MJHOzich28aP4c+Hc/du7DS+ZPeNtg0t2zvL4qTWlvXFT9alU02opLAkh+ELePn
+JnrABNd98nz+tyvJpQzJfuTqgD8pyC1uouZTdHMS9/DHBZ3Pd4hESY9W2USVle1sgr1GMrDk/Emw
+tN2bNGTEA6a2uItueKl8hzHy80mJUQt4/mGUy5exCtY6OQq+8otxbZcvGx1Af5+K126+1Qayixfx
+Hwv+u9jW9tbN16ZKT+3BKuCeDvz8gugLPdPgfwIMAOEbe4vgJZGyAAAAAElFTkSuQmCC
+__theme/bluerain/css/header_bg.png__
+iVBORw0KGgoAAAANSUhEUgAAAAEAAABsCAMAAACcoSg/AAAABGdBTUEAAK/INwWK6QAAABl0RVh0
+U29mdHdhcmUAQWRvYmUgSW1hZ2VSZWFkeXHJZTwAAAEXUExURUB70ixVjjtxwDJgoUB60EN/2j10
+xkJ+1y5ZlSxVjT94zSxWj0J+2EF81C1XkTNipjRlqjpwvjxzwzxywi5Yk0J/2TZosC5aljtywS9c
+mi1Ykj53yj52yj52yTFenTltuC1XkDhstzBcmkF91UJ91j95zi1XkkF91j11yEF70zx0xThrtjlu
+ujNipTZnrzRkqStVjDBdnC9bmTVmrTRkqDhrtTVmrDNjpztwvz11xytUjC9alzpvvTVnrjdqszhs
+uDFfnzdqtDRjpzZor0N/2TxzxEF81TJhoi9blzdpsjltuTFenjJho0B60T53yyxWkC5ZlDVlqz95
+zzpvvDNhpDBdmy1YkzFfoDdpsTBdnTluuz53zC9bmBhZsZMAAACUSURBVHjaFMGHIgIAFADAQ2Ul
+O7Oy9x4ZIdkz2Yn//46eO8e2dMaW2B1fTMnEBeO+DKnpiCNK7vzZMWbGp20/8h6d+9YWbz35sK7X
+gQt7NvV5VbTr1IZLWXX3zlTsWzWtrODBmoZ3J/odao2jBg24tiIVjyzJmTSh6ldXnPcmEZ/9W9YT
+b8xaNKc9puOwK0nJpgADAKcKESASzMvVAAAAAElFTkSuQmCC
+__theme/bluerain/template/tt2/kwiki_screen.html__
+[%- INCLUDE kwiki_doctype.html %]
+[% INCLUDE kwiki_begin.html %]
+
+ <div id="main">
+  <div id="header">
+   <h1>[% screen_title || self.class_title %]</h1>
+  </div>
+  <div id="menu">
+    <!--  <ul><li class="selected"><a href="./index.html">Home</a></li></ul> -->
+    [% hub.toolbar.html %]
+  </div>
+  <div id="content">
+   <div class="article">
+    [% INCLUDE $content_pane %]
+   </div>
+  </div>
+  <div id="panel">
+   <div class="left">
+    [% IF hub.have_plugin('user_name') %][% INCLUDE user_name_title.html %][% END %]
+    <div id="widgets_pane">[% hub.widgets.html %]</div>
+   </div>
+   <div class="right">
+    <div id="status_pane"> [% hub.status.html %] </div>
+   </div>
+  </div>
+ <div id="footer"><p>Designed by <a href="http://www.pixelcarnage.com/">PixelCarnage</a>.</p></div>
+
+[% INCLUDE kwiki_end.html -%]
+
+__theme/bluerain/css/kwiki.css__
+* {
+	margin : 0;
+	padding : 0;
+}
+
+a {
+	color : #437fda;
+	text-decoration : none;
+}
+a:visited {
+	color : #437fda;
+	text-decoration : underline;
+}
+a:hover {
+	color : #ba8f43;
+}
+
+h2 {
+	color : #343434;
+	font : italic 200% sans-serif;
+}
+h3 {
+	color : #343434;
+	font : italic 160% sans-serif;
+}
+h4 {
+	color : #343434;
+	font : bold italic 110% sans-serif;
+	padding : 1em 1em 0 1em;
+}
+
+html {
+	color : #565656;
+	font : 70%/170% sans-serif;
+	text-align : justify;
+}
+
+img {
+	margin : 1em 1em 0 1em;
+}
+img.left {
+	float : left;
+	margin : 1em 1em 0 0;
+}
+img.right {
+	float : right;
+	margin : 1em 0 0 1em;
+}
+
+blockquote {
+	font-style : italic;
+	margin : 1em 1em 0 1em;
+	padding : 0 0 1em 0;
+}
+blockquote span {
+	font-size : 200%;
+	line-height : 1%;
+	margin : 0 0.15em;
+	position : relative;
+	top : 0.25em;
+}
+
+form button {
+	background : #ffffff;
+	border : 1px solid #cfcfcf;
+	padding : 0.25em;
+	margin : 0 0 0 0.75em;
+}
+form input {
+	border : none;
+	width : 100%;
+}
+form textarea {
+	border : none;
+	width : 100%;
+	height : 10em;
+}
+form p.button {
+	text-align : right;
+}
+form p.input,
+form p.text {
+	background : #ffffff;
+	border : 1px solid #cfcfcf;
+	padding : 0.25em;
+	margin : 0.25em 1em 0 1em;
+}
+
+p {
+	padding : 1em 1em 0 1em;
+}
+
+ul,
+ol {
+	padding : 1em 1em 0 3em;
+}
+
+#main {
+	margin : auto;
+	max-width : 65em;
+	min-width : 40em;
+	width : auto !important;
+	width : 65em;
+}
+
+#header {
+	background : #2b548c url('../images/header_bg.png') repeat-x bottom left;
+	padding : 6em 4em 1em 4em;
+}
+#header h1 {
+	color : #ffffff;
+	font : italic 200% sans-serif;
+}
+
+#menu {
+	background : #437fda;
+	border-bottom : 1px solid #2b548c;
+	font : 100% sans-serif;
+}
+#menu ul {
+	padding : 0.75em 4em;
+}
+#menu li {
+	display : inline;
+}
+#menu li a {
+	color : #ffffff;
+	padding : 0.75em 1.5em;
+}
+#menu li a:hover {
+	background : #2b548c;
+}
+#menu li.selected a {
+	background : #ffffff;
+	border : 1px solid #2b548c;
+	border-bottom : 1px solid #ffffff;
+	color : #437fda;
+}
+#menu li.selected a:hover {
+	background : #ffffff;
+	color : #ba8f43;
+}
+
+#content {
+	border-bottom : 1px solid #cfcfcf;
+	height : auto !important;
+	height : 1%;
+	overflow : hidden;
+	padding : 2em 0 0 0;
+}
+#content div {
+	padding : 0 4em 2em 4em;
+}
+
+#panel {
+	background : #efefef;
+	border : 1px solid #dfdfdf;
+	border-bottom : 1px solid #cfcfcf;
+	border-top : none;
+	height : auto !important;
+	height : 1%;
+	overflow : hidden;
+	padding : 0 5em 2em 5em;
+}
+#panel div {
+	padding : 2em 0 0 0;
+}
+div.left {
+	left : -1em;
+	float : left;
+	position : relative;
+	width : 50%;
+}
+div.right {
+	left : 1em;
+	float : left;
+	position : relative;
+	width : 50%;
+}
+
+#footer {
+	font-size : 85%;
+	margin : auto;
+	padding : 1em 0 3em 0;
+	text-align : center;
+	width : 65%;
+}
