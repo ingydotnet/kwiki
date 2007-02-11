137a193fa0b20cb270d81c0317c214e9a5718fac
miyagawa <miyagawa@4b3b33bc-d223-0410-9309-b363ae52eedd>
2007-02-11 05:00:29 +0000

    shiny Kwiki::FastCGI
    git-svn-id: http://svn.kwiki.org/kwiki/trunk@141 4b3b33bc-d223-0410-9309-b363ae52eedd

---
+++ src/plugins/miyagawa/Kwiki-FastCGI/lib/Kwiki/FastCGI.pm
@@ -0,0 +1,48 @@
+package Kwiki::FastCGI;
+use Kwiki::Files -Base;
+
+1;
+
+__DATA__
+
+__index.fcgi__
+#!/usr/bin/perl
+use lib 'lib';
+use Kwiki::Boot;
+use CGI::Fast;
+
+while ( my $cgi = CGI::Fast->new ) {
+    Kwiki::Boot->debug->class->new->kwiki->process;
+}
+
+__END__
+
+=head1 NAME
+
+Kwiki::FastCGI - FastCGI bootstrap file template for Kwiki
+
+=head1 SYNOPSIS
+
+  > kwiki -add Kwiki::FastCGI
+
+  # httpd.conf or .htaccess
+  AddHandler fastcgi-script .fcgi
+
+=head1 DESCRIPTION
+
+Kwiki::FastCGI installs C<index.fcgi> to run your Kwiki as a FastCGI server.
+
+=head1 AUTHOR
+
+Tatsuhiko Miyagawa E<lt>miyagawa@bulknews.netE<gt>
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+L<CGI::Fast>, L<Kwiki>
+
+=cut
