12c0a4e460bb51d1bedcc748f71ef465863f838c
ingy <ingy@4b3b33bc-d223-0410-9309-b363ae52eedd>
2007-02-20 21:08:30 +0000

    r3426@skinny:  ingy | 2007-02-20 13:07:17 -0800  Changing kwiki.env to _kwiki
    git-svn-id: http://svn.kwiki.org/kwiki/trunk@157 4b3b33bc-d223-0410-9309-b363ae52eedd

--- src/plugins/miyagawa/Kwiki-FastCGI/lib/Kwiki/FastCGI.pm
+++ src/plugins/miyagawa/Kwiki-FastCGI/lib/Kwiki/FastCGI.pm
@@ -45,7 +45,7 @@ __index.fcgi__
 use strict;
 BEGIN {
     my $fh;
-    for (qw(kwiki.env .ht-kwiki.env)) { open $fh, $_ and last }
+    for (qw(_kwiki .ht_kwiki)) { open $fh, $_ and last }
     do { $ENV{$1} = $2 if /^(\w+)\s*=\s*['"]?(.*?)['"]?\s*$/ } for <$fh>;
 }
 use lib grep { -e } split /:/, $ENV{KWIKI_LIB_PATH} || 'lib';
