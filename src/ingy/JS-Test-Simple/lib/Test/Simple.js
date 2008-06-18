7409e4afa0e760083af62efff0624a48d4927515
ingy <ingy@4b3b33bc-d223-0410-9309-b363ae52eedd>
2008-06-18 22:41:01 +0000

    Test-Simple for JS
    git-svn-id: http://svn.kwiki.org/kwiki/trunk@375 4b3b33bc-d223-0410-9309-b363ae52eedd

---
+++ src/ingy/JS-Test-Simple/lib/Test/Simple.js
@@ -0,0 +1,31 @@
+// $Id: /mirror/Test.Simple-trunk/lib/Test/Simple.js 1302 2008-04-15T16:17:47.318367Z theory  $
+
+/*global JSAN, Test */
+
+// Set up package.
+if (typeof JSAN != 'undefined') JSAN.use('Test.Builder');
+else {
+    if (typeof Test == 'undefined' || typeof Test.Builder == 'undefined')
+        throw new Error(
+            "You must load either JSAN or Test.Builder "
+            + "before loading Test.Simple"
+        );
+}
+
+Test.Simple = {};
+Test.Simple.EXPORT      = ['plan', 'ok'];
+Test.Simple.EXPORT_TAGS = { ':all': Test.Simple.EXPORT };
+Test.Simple.VERSION     = '0.23';
+
+Test.Simple.plan = function (cmds) {
+    return Test.Simple.Test.plan(cmds);
+};
+
+Test.Simple.ok = function (val, desc) {
+    return Test.Simple.Test.ok(val, desc);
+};
+
+// Handle exporting.
+if (typeof JSAN == 'undefined') Test.Builder.exporter(Test.Simple);
+
+Test.Simple.Test = Test.Builder.instance();
